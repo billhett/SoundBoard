871ab7b1fafcadabcae6c32d3bfc15a94c193321
William Hettinger <bill.hettinger@gmail.com>
2018-03-02 15:54:52 -0500

    add second view controller with buttons and textfield

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by William Hettinger on 3/2/18.
+//  Copyright © 2018 William Hettinger. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+
+}
